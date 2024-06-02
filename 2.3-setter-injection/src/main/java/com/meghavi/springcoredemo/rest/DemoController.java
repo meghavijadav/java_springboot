cc445ace7e6ff6c27a2ee6611c498a4e1a3d8a19
Meghavi Jadav <100013496+meghavijadav@users.noreply.github.com>
2024-06-02 17:08:08 +0530

    commit

---
+++ 2.3-setter-injection/src/main/java/com/meghavi/springcoredemo/rest/DemoController.java
@@ -0,0 +1,29 @@
+package com.meghavi.springcoredemo.rest;
+
+import com.meghavi.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    //define a private field for dependency
+    private Coach myCoach; //Field Injection
+
+    @Autowired
+    public void setCoach(Coach theCoach){
+        myCoach = theCoach;
+    }
+
+//      You could use any method instead of setCoach and it would still work
+//    @Autowired
+//    public void doSomeStuff(Coach theCoach){
+//        myCoach = theCoach;
+//    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}
+
