bdea6297671abd6ce6ea31cc8251728d98228431
Meghavi Jadav <100013496+meghavijadav@users.noreply.github.com>
2024-06-02 14:35:55 +0530

    commit

---
+++ 2.2-component-scanning/src/main/java/com/meghavi/springcoredemo/SpringcoredemoApplication.java
@@ -0,0 +1,18 @@
+package com.meghavi.springcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication(
+//		scanBasePackages = {"com.meghavi.springcoredemo",
+//							"com.meghavi.util"}
+//)
+
+@SpringBootApplication
+public class SpringcoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoredemoApplication.class, args);
+	}
+
+}
